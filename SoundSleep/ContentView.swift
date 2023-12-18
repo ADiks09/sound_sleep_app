9e156be0f3de20e1e97507405845aae24bb6e602
ADiks09 <antondikson@gmail.com>
2023-12-19 00:56:56 +0100

    WIP

--- SoundSleep/ContentView.swift
+++ SoundSleep/ContentView.swift
@@ -2,13 +2,50 @@ import SwiftUI
 
 internal struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            content(title: "Discover")
+                .tabItem {
+                    Label("Discover", systemImage: "moon.stars.fill")
+                }
+            content(title: "Composer")
+                .tabItem {
+                    Label("Composer", systemImage: "music.note")
+                }
+            content(title: "Profile")
+                .tabItem {
+                    Label("Profile", systemImage: "person.fill")
+                }
         }
-        .padding()
+        .onAppear {
+            let appearance = UITabBarAppearance()
+            appearance.backgroundColor = UIColor.Dark.secondary
+            UITabBar.appearance().standardAppearance = appearance
+            UITabBar.appearance().scrollEdgeAppearance = appearance
+        }
+    }
+
+    private func content(title: String) -> some View {
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    Text(title)
+                        .foregroundStyle(.white)
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.Dark.primary)
+            .navigationTitle(Text(title))
+        }
+        .onAppear {
+            let appearance = UINavigationBarAppearance()
+            appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+            appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            appearance.backgroundColor = UIColor.Dark.primary
+            UINavigationBar.appearance().standardAppearance = appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+            UINavigationBar.appearance().compactScrollEdgeAppearance = appearance
+            UINavigationBar.appearance().compactAppearance = appearance
+        }
     }
 }
 
